6cbfcc012aefe70a99467ef35fa6fe4954e9c427
pf035 <pawelfranka@gmail.com>
2023-12-11 19:46:07 +0100

    zm

--- LC2/LC2/Program.cs
+++ LC2/LC2/Program.cs
@@ -1,21 +1,24 @@
 ﻿// See https://aka.ms/new-console-template for more information
+//int number;
+//string numberAsString = number.ToString();
+//char[] letters = numberAsString.ToArray();
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.Design;
 using System.Diagnostics.Metrics;
 
-const int c1 = 48; //wartość dziesiętna cyfry 0 w kodzie ASCII
-const int c2 = 10; //liczba cyfr w systemie 10
+const int chASCII = 48; //wartość dziesiętna cyfry 0 w kodzie ASCII
+const int numDig10 = 10; //liczba cyfr w systemie 10
 
-String licCal;
-byte[] tab = new byte[c2];
+String ourNum;
+byte[] tab = new byte[numDig10];
 
 Console.WriteLine("Wprowadź liczbę: ");
-licCal = Console.ReadLine();
+ourNum = Console.ReadLine();
 
-for (int i = 0; i < licCal.Length; i++) {
-    for (int j = 0; j < c2; j++)
+for (int i = 0; i < ourNum.Length; i++) {
+    for (int j = 0; j < numDig10; j++)
     {
-        if (((int)licCal[i]) == j + c1) 
+        if (((int)ourNum[i]) == j + chASCII) 
         {
             tab[j] += 1;
             break;
@@ -23,11 +26,11 @@ for (int i = 0; i < licCal.Length; i++) {
     };   
 };
     
-Console.WriteLine("Wyniki dla liczby: " + licCal);
+Console.WriteLine("Wyniki dla liczby: " + ourNum);
 
 string zm1 = "";
 
-for (int i = 0; i < c2; i++) { 
+for (int i = 0; i < numDig10; i++) { 
     zm1 = "";
     if ((int)tab[i] > 0) {
         zm1 = zm1 + tab[i];
